fb468536ca3e4ea081d88de20655eb44d793b37b
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-07-22 13:51:47 -0300

    configurando modelo de lote

--- app/Models/Lote.php
+++ app/Models/Lote.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Lote extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $table = "lote";
+    public $timestamps = false;
+
+    public function Productos() {
+        return $this -> hasMany(Producto::class);
+    } 
 }
