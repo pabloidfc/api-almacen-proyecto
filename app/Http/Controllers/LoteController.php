dd4a945a721373fb86d18c67fd39f3a492a1caea
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-09-22 15:00:19 -0300

    agregando validaciones al modificar Lotes, cambiando enpoints

--- app/Http/Controllers/LoteController.php
+++ app/Http/Controllers/LoteController.php
@@ -33,7 +33,7 @@ class LoteController extends Controller
         $lote -> Almacen() -> associate($req -> almacen_destino);
         $lote -> Creador() -> associate($req -> creador_id);
         
-        $productos = Producto::whereIn('id', $idsProductos)->get();
+        $productos = Producto::whereIn('id', $idsProductos)->get(); 
         $lote -> peso = $productos->sum('peso');
         $lote -> save();
         $lote->Productos()->saveMany($productos);
@@ -64,7 +64,7 @@ class LoteController extends Controller
     }
 
     public function ListarLoteProductos(Request $req, $idLote) {
-        $lote = Lote::find($idLote);
+        $lote = Lote::findOrFail($idLote);
         
         if ($lote) {
             $lote -> Productos;
@@ -75,7 +75,7 @@ class LoteController extends Controller
     }
 
     public function ListarAlmacenDestino(Request $req, $idLote) {
-        $lote = Lote::find($idLote);
+        $lote = Lote::findOrFail($idLote);
         
         if ($lote) {
             $lote -> Almacen;
@@ -86,31 +86,44 @@ class LoteController extends Controller
     }
 
     public function Modificar(Request $req, $idLote) {
-        $lote = Lote::find($idLote);
+        $lote = Lote::findOrFail($idLote);
 
-        if ($lote) {
-            if ($req -> has("peso"))            $lote -> peso            = $req -> post("peso");
-            if ($req -> has("estado"))          $lote -> estado          = $req -> post("estado");
-            if ($req -> has("almacen_destino")) $lote -> almacen_destino = $req -> post("almacen_destino");
-    
-            $lote -> save();
-            return $lote;
-        }
+        $validaciones = Validator::make($req->all(), [
+            "creador_id"      => ["required", "integer", Rule::exists('users', 'id')],
+            "almacen_destino" => ["required", "integer", Rule::exists('almacen', 'id')],
+            "estado"          => "nullable|in:Creado,En viaje,Desarmado",
+            "idsProductos"    => "required|array", 
+            "idsProductos.*"  => "exists:producto,id"
+        ]);
+
+        if($validaciones->fails())
+            return response($validaciones->errors(), 400);
         
+        $idsProductos = $req -> input("idsProductos", []);
+    
+        $lote -> estado = $req -> input("estado");
+        
+        $lote -> Almacen() -> associate($req -> almacen_destino);
+        $lote -> Creador() -> associate($req -> creador_id);
+        
+        $productos = Producto::whereIn('id', $idsProductos)->get();
+        $lote -> peso += $productos->sum('peso');
+        $lote -> save();
+        $lote->Productos()->saveMany($productos);
+        return $lote;
+
         return response(["msg" => "Lote no encontrado!"], 404);
     }
     
     public function Desarmar(Request $req, $idLote) {
-        $lote = Lote::find($idLote);
+        $lote = Lote::findOrFail($idLote);
         
         if ($lote) {
             $productos = $lote -> Productos;
             
-            if ($productos) {
-                foreach ($productos as $producto) {
-                    $producto -> lote_id = null;
-                    $producto -> save();
-                }
+            foreach ($productos as $producto) {
+                $producto -> lote_id = null;
+                $producto -> save();
             }
             
             $lote -> estado = "Desarmado";
@@ -123,17 +136,10 @@ class LoteController extends Controller
     }
 
     public function Eliminar(Request $req, $idLote) {
-        $lote = Lote::find($idLote);
-        
-        if ($lote) {
-            if ($lote -> Productos() -> count() > 0) {
-                return response(["msg" => "El Lote no se ha podido eliminar!"], 400);
-            };
+        $lote = Lote::findOrFail($idLote);
             
-            $lote -> delete();
-            return ["msg" => "El Lote ha sido eliminado correctamente!"];
-        }
+        $lote -> delete();
+        return ["msg" => "El Lote ha sido eliminado correctamente!"];
 
-        return response(["msg" => "Lote no encontrado!"], 404);
     }
 }
